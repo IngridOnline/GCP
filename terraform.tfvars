084da8e6a72fce2a98150999f6a5a4dd3adab026
IngridOnline <ingrid.finnoy@soprasteria.com>
2021-08-13 09:35:31 +0200

    first commit

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project                  = "terraform-learning-322608"
+credentials_file         = "terraform-learning-322608-2fb43edf64e8.json"
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "europe-north1"
+}
+
+variable "zone" {
+  default = "europe-north1-a"
+}
+
+variable "project" { 
+    //default = "terraform-learning-322608"
+}
+
+variable "credentials_file" { 
+    //default = "terraform-learning-322608-2fb43edf64e8.json"
+}
